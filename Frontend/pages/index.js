6c130658dc9f8faa707df6628cafadbcc5495bca
Sebastian Gutierrez <segut10@gmail.com>
2020-12-18 13:03:01 -0500

    subiendo la base del proyecto front

---
+++ Frontend/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            Señores Bienvenidos al proyecto a darle con toda
+        </div>
+    )
+}
+
+export default Home
